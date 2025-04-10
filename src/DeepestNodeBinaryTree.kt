54ed1d0a50f733d6a79a77d28e8dd1ea92386482
Adam Boerema <adamboerema@gmail.com>
2025-04-10 17:17:06 -0400

    Added max depth challenge and deepest node link

--- src/DeepestNodeBinaryTree.kt
+++ src/DeepestNodeBinaryTree.kt
@@ -1,1 +1,20 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
 
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+data class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+fun lcaDeepestLeaves(root: TreeNode?): TreeNode? {
+}
---
+++ src/MaxDepth.kt
@@ -0,0 +1,16 @@
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+fun maxDepth(root: TreeNode?): Int {
+    return 0
+}
