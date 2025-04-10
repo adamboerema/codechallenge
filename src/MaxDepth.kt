43b5a028773daa6d70a416d220fdbb6bd1b545c5
Adam Boerema <adamboerema@gmail.com>
2025-04-10 18:11:26 -0400

    working on maxdepth

--- src/MaxDepth.kt
+++ src/MaxDepth.kt
@@ -11,6 +11,18 @@
  *     var right: TreeNode? = null
  * }
  */
+
+data class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
 fun maxDepth(root: TreeNode?): Int {
+    var maxDepth = 0
+    searchDepth(root?)
     return 0
+}
+
+fun searchDepth(node: TreeNode?, maxDepth: Int) {
+
 }
